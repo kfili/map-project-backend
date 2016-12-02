0867ca811177be0c5a98d5156482f35149b6cf61
kfili <kosta.filiotis@gmail.com>
2016-12-02 14:10:51 -0500

    prep places controller for testing and crud

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,58 +1,60 @@
-# frozen_string_literal: true
-class PlacesController < OpenReadController
-  before_action :set_place, only: [:update, :destroy]
+class PlacesController < ApplicationController
+    before_action :set_place, only: [:show, :update, :destroy]
 
-  # GET /places
-  # GET /places.json
-  def index
-    @places = Place.all
+    # GET /places
+    # GET /places.json
+    def index
+        @places = Place.all
 
-    render json: @places
-  end
-
-  # GET /places/1
-  # GET /places/1.json
-  def show
-    render json: Place.find(params[:id])
-  end
-
-  # POST /places
-  # POST /places.json
-  def create
-    @place = current_user.places.build(place_params)
-
-    if @place.save
-      render json: @place, status: :created
-    else
-      render json: @place.errors, status: :unprocessable_entity
+        render json: @places
     end
-  end
 
-  # PATCH/PUT /places/1
-  # PATCH/PUT /places/1.json
-  def update
-    if @place.update(place_params)
-      head :no_content
-    else
-      render json: @place.errors, status: :unprocessable_entity
+    # GET /places/1
+    # GET /places/1.json
+    def show
+        render json: @place
     end
-  end
 
-  # DELETE /places/1
-  # DELETE /places/1.json
-  def destroy
-    @place.destroy
+    # POST /places
+    # POST /places.json
+    def create
+        @place = Place.new(place_params)
 
-    head :no_content
-  end
+        if @place.save
+            render json: @place, status: :created, location: @place
+        else
+            render json: @place.errors, status: :unprocessable_entity
+        end
+    end
 
-  def set_place
-    @place = current_user.places.find(params[:id])
-  end
+    # PATCH/PUT /places/1
+    # PATCH/PUT /places/1.json
+    def update
+        @place = Place.find(params[:id])
 
-  def place_params
-    params.require(:place).permit(:text)
-  end
+        if @place.update(place_params)
+            head :no_content
+        else
+            render json: @place.errors, status: :unprocessable_entity
+        end
+    end
 
-  private :set_place, :place_params
+    # DELETE /places/1
+    # DELETE /places/1.json
+    def destroy
+        @place.destroy
+
+        head :no_content
+    end
+
+    private
+
+    def set_place
+        @place = Place.find(params[:id])
+    end
+
+    def place_params
+        params.require(:place)
+              .permit(:name, :type, :location, :address, :phone, :rating)
+    end
 end
