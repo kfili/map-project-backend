55dc96f73597d6c6fa4464d0232b906c29ed1904
kfili <kosta.filiotis@gmail.com>
2016-12-04 11:35:20 -0500

    add functionality of users own their own data
    Fix so create method in controller still works

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
-  post '/places' => 'places#create'
-  get '/places' => 'places#index'
-  patch '/places/:id' => 'places#update'
-  delete '/places/:id' => 'places#destroy'
   resources :places, except: [:new, :edit]
   resources :examples, except: [:new, :edit]
   post '/sign-up' => 'users#signup'
---
+++ db/migrate/20161204004948_add_user_id_to_places.rb
@@ -0,0 +1,5 @@
+class AddUserIdToPlaces < ActiveRecord::Migration
+  def change
+    add_reference :places, :user, index: true, foreign_key: true
+  end
+end
